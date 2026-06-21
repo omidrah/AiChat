955b0eb16f839cd965b9a6c1690ae437a5b94d5a
reza <reza@email.com>
2026-06-21 08:39:47 +0330

    add environmet in frontend

---
+++ Frontend/ai-chat-web/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://api.pada.com/api',
+//   hubUrl: 'https://api.pada.com/hubs/chat'
+// };
+
+export const environment = {
+  production: true,
+  apiUrl: 'http://132.168.250.205:7117/api',
+  hubUrl: 'http://132.168.250.205:7117/hubs/chat'
+};
