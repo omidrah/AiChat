bcf14b955ac09e28874b2de381e48897172c2a8c
omidrhm <omid_rhm@yahoo.com>
2026-06-22 11:53:54 +0330

    frontend: add authentication service

---
+++ Frontend/ai-chat-web/src/app/services/AuthService.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+    baseUrl = environment.apiUrl;
+
+    constructor(private http: HttpClient) { }
+
+    getMode() {
+        return this.http.get<{ mode: string }>(`${this.baseUrl}/mode`);
+    }
+
+    login(userName: string, password: string) {
+        return this.http.post<any>(`${this.baseUrl}/login`, { userName, password })
+            .pipe(
+                tap(res => {
+                    localStorage.setItem('token', res.accessToken);
+                })
+            );
+    }
+
+    getMe() {
+        return this.http.get(`${this.api}/me`);
+    }
+
+    getToken() {
+        return localStorage.getItem('token');
+    }
+
+    logout() {
+        localStorage.removeItem('token');
+    }
+}
