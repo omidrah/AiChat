94df76602a7516f9b73bac28ab5c38bc4bbad449
omidrhm <omid_rhm@yahoo.com>
2026-06-29 12:24:43 +0330

    resolve problem of routing

--- Frontend/ai-chat-web/src/app/app.ts
+++ Frontend/ai-chat-web/src/app/app.ts
@@ -2,46 +2,70 @@ import { Component, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { ConversationList } from './conversations/conversation-list/conversation-list';
 import { AuthService } from './services/AuthService';
+import { firstValueFrom } from 'rxjs';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,  ConversationList],
+  imports: [RouterOutlet, ConversationList],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  
+
 })
 
 export class App {
-  
+
   protected readonly title = signal('ai-chat-web');
 
-  constructor(private auth:AuthService, private router:Router){}
+  constructor(private auth: AuthService, private api: ApiService, private router: Router) { }
 
-  
-  dark=false;
+  dark = false;
 
-  toggleTheme(){
-    this.dark=!this.dark;
+  toggleTheme() {
+    this.dark = !this.dark;
 
-    if(this.dark)
+    if (this.dark)
       document.body.classList.add("dark");
     else
       document.body.classList.remove("dark");
   }
-ngOnInit() {
-  this.auth.getMode().subscribe(x => {
-    if (x.mode === 'Local') {
-      if (this.auth.isLoggedIn()) {
-        this.router.navigate(['/chat']);
-      } else {
-        this.router.navigate(['/login']);
-      }
 
+  async ngOnInit() {
+
+    const mode = await firstValueFrom(this.auth.getMode());
+
+    if (mode.mode === 'Local' && !this.auth.isLoggedIn()) {
+      this.router.navigate(['/login']);
       return;
     }
 
-    this.router.navigate(['/chat']);
-  });
-}
+    try {
+
+      const conversations = await firstValueFrom(this.api.getConversations());
+
+      if (conversations.length > 0) {
+
+        this.router.navigate([
+          '/chat',
+          conversations[0].id
+        ]);
+
+        return;
+      }
+
+      const id = await firstValueFrom(this.api.createConversation());
+
+      this.router.navigate([
+        '/chat',
+        id
+      ]);
+
+    }
+    catch {
+
+      this.router.navigate(['/login']);
+
+    }
+  }
 
 }
